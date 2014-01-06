965894a7df43350e44e73537cf10b4f8b34121c4
SDT1 <eyebeam79@naver.com>
2014-01-06 13:33:49 +0900

    P.140 실습을 완료했습니다.

--- PickerCountdown/ViewController.m
+++ PickerCountdown/ViewController.m
@@ -9,10 +9,44 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+- (void)startTimer;
+- (void)stopTimer;
 @end
 
 @implementation ViewController
+{
+    NSTimer *timer;
+    bool timerWorking;
+}
+
+// 각 초마다 1분씩 감소
+- (void)tickDown:(NSTimer *)timer
+{
+    self.datePicker.countDownDuration = self.datePicker.countDownDuration - 60;
+    
+    // 1분이내면 정지
+    if (self.datePicker.countDownDuration <= 60)
+    {
+        NSLog(@"Done!");
+        [self stopTimer];
+    }
+}
+
+// 타이머시작
+- (void)startTimer
+{
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(tickDown:) userInfo:nil repeats:YES];
+}
+
+// 타이머정지
+- (void)stopTimer
+{
+    [timer invalidate];
+    timer = nil;
+    timerWorking = NO;
+}
+
 
 - (void)viewDidLoad
 {
@@ -24,6 +58,23 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)togleTimer:(id)sender
+{
+    // 버튼토글 설정
+    NSString *title = timerWorking ? @"Start" : @"Stop";
+    [sender setTitle:title forState:UIControlStateNormal];
+    timerWorking = !timerWorking;
+    
+    if (timerWorking)
+    {
+        [self startTimer];
+    }
+    else
+    {
+        [self stopTimer];
+    }
 }
 
 @end
